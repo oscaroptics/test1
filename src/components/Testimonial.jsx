042b6a64db0df1cfea1df8caf124daf6b7edf8a2
oscaroptics <arashutosh01@gmail.com>
2023-07-06 03:33:29 +0530

    2nd commit

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Testimonial = ({ name, testimonial, image }) => {
+const Testimonial = ({ name, testimonial, image, index, total }) => {
   return (
     <div className="testimonial">
       <div className="testimonial-content">
-        <h2 className='h2div'>Success Stories</h2>
+        <h2 className="h2div">Success Stories</h2>
         <h1 className="h1div">{name}</h1>
         <p className="pdiv">{testimonial}</p>
         <button>Read More Stories</button>
+        {/* <h3>{`${index + 1}/${total}`}</h3> */}
       </div>
       <div className="testimonial-image">
         <img src={image} alt={name} />
--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -9,27 +9,53 @@ const testimonialData = [
     image: "img1.jpg",
   },
   {
-    name: "John Doe",
+    name: "Jane Doe",
     testimonial:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur risus vitae orci ultricies consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur risus vitae orci ultricies consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur risus vitae orci ultricies consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur risus vitae orci ultricies consequat.",
+      "Sed consectetur risus vitae orci ultricies consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur risus vitae orci ultricies consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur risus vitae orci ultricies consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur risus vitae orci ultricies consequat.",
+    image: "img2.jpg",
+  },
+  {
+    name: "Mike Doe",
+    testimonial: "Lorem vitae orci ultricies consequat. consequat.",
     image: "img2.jpg",
   },
 ];
 
 const TestimonialSection = () => {
+  const [testimonialIndex, setTestimonialIndex] = React.useState(0);
+  const numTestimonials = testimonialData.length; // 2
+
+  const handleOnClick = () => {
+    if (testimonialIndex + 1 >= numTestimonials) {
+      setTestimonialIndex(0);
+    } else {
+      setTestimonialIndex(testimonialIndex + 1);
+    }
+  };
+
+  React.useEffect(() => {
+    let id = setInterval(() => {
+      handleOnClick();
+    }, 5000);
+    return () => clearInterval(id);
+  }, [, testimonialIndex]);
+
   return (
-    <div className="testimonial-section">
-      <div className="swiper-container">
-        <div className="swiper-wrapper">
-          {testimonialData.map((testimonial, index) => (
-            <div className="swiper-slide" key={index}>
-              <Testimonial {...testimonial} />
-            </div>
-          ))}
-        </div>
-        <div className="swiper-pagination"></div>
+    <>
+      <div className="testimonial-section">
+        <Testimonial {...testimonialData[testimonialIndex]} />
+        {testimonialData.map((_data, index) => (
+          <button
+            style={{
+              backgroundColor: testimonialIndex === index ? "red" : "gray",
+            }}
+            onClick={() => setTestimonialIndex(index)}
+          >
+            {index + 1}
+          </button>
+        ))}
       </div>
-    </div>
+    </>
   );
 };
 
